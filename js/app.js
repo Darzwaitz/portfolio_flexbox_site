99fec80becf386c47e28ac85b0f16d7edb11cd6c
darzwaitz <darzwaitz@yahoo.com>
2022-01-11 15:52:02 +0000

    creating nav menu for phone size - part 2

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 const hamburgerContainer = document.querySelector('.header__main-nav') ;
 const hamburger = document.querySelector('.header__main-nav--hamburger') ;
-const links = document.querySelector('.header__main-nav--links li') ;
+const links = document.querySelectorAll('.header__main-nav--links li') ;
 
-hamburger.addEventListener('click', ()=>{hamburgerContainer.classList.toggle("clicked")});
+hamburger.addEventListener('click', ()=>{
+    hamburgerContainer.classList.toggle("clicked");
+    links.forEach((link) => {
+        link.classList.toggle('fade');
+        console.log(link);
+    });
+});
